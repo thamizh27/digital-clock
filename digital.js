f4b2d300ab850270922cfa8475a2863b5bc1b519
Thamizharasan Devaraj <cdkthamizh27@gmail.com>
2020-10-24 12:11:35 +0530

    added js file

---
+++ digital.js
@@ -0,0 +1,27 @@
+window.onload = function tham() {
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let session = "AM";
+
+    if (h == 0) {
+        h = 12;
+    }
+
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? `0${h}`: h;
+    m = (m < 10) ? `0${m}`: m;
+    s = (s < 10) ? `0${s}`: s;
+
+    let time = h + " : " + m + " : " + s + " " + session;
+
+    document.querySelector(".clock").innerText = time;
+    document.querySelector(".clock").textContent = time;
+
+    setTimeout(tham, 1000)
+} 
